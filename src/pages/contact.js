422557ca380fc46a364ac159ebdc71441bc522f8
Sacha Wellborn <34571833+recluso@users.noreply.github.com>
2020-05-21 20:20:29 +0100

    another form update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,27 +20,28 @@ const ContactPage = ({
           <p className="dark-text">&rarr; &rarr; &rarr;</p>
         </div>
         <div>
-          <form className="form-container" name="contact" method="POST" data-netlify="true">
-  <div>
-    <label for="name">Name</label>
-    <input type="text" name="name" />
-  </div>
-  <div>
-    <label for="emailsender">Email</label>
-    <input type="email" name="emailsender" />
-  </div>
-  <div>
-    <label for="subject">Subject</label>
-    <input type="text" name="subject" />
-  </div>
-  <div>
-    <label for="message">Message</label>
-    <textarea name="message"></textarea>
-  </div>
-  <div style={{display: "flex", justifyContent: "flex-end"}}>
-    <input type="submit" className="button -primary" style={{marginRight: 0}} />
-  </div>
-</form>
+          <form className="form-container" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+            <input type="hidden" name="form-name" value="contact" />
+            <div>
+              <label for="name">Name</label>
+              <input type="text" name="name" />
+            </div>
+            <div>
+              <label for="emailsender">Email</label>
+              <input type="email" name="emailsender" />
+            </div>
+            <div>
+              <label for="subject">Subject</label>
+              <input type="text" name="subject" />
+            </div>
+            <div>
+              <label for="message">Message</label>
+              <textarea name="message"></textarea>
+            </div>
+            <div style={{display: "flex", justifyContent: "flex-end"}}>
+              <input type="submit" className="button -primary" style={{marginRight: 0}} />
+            </div>
+          </form>
     
         </div>
       </div>
