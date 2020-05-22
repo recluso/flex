4580a498f11e3510d5f38d410af88c39efd56b24
Sacha Wellborn <swellborn@epilepsy.org.uk>
2020-05-22 12:45:09 +0100

    trying to change the home, add strava thing

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -17,10 +17,15 @@ export default () => (
     render={data => (
       <div className="hero-header">
         <div className="headline">{data.site.siteMetadata.home.title}</div>
-        <div 
-          className="primary-content" 
-          dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
-        />
+        <div className="frontcontain">
+          <div
+            className="primary-content"
+            dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
+          />
+          <div className="stravaList">
+            <iframe height='454' width='300' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/7199344/latest-rides/b54e44f1bad55fe91ba5857e0377e45d9526665f'></iframe>
+          </div>
+        </div>
         <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
       </div>
     )}
